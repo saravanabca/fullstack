a14558c81653d8a1dc10863eab80cce7b8c7766e
elango14 <elango@appteq.in>
2023-08-18 14:04:59 +0530

    login validation and auth

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,85 +1,222 @@
-import { React } from "react";
+import { React, useState, useEffect } from "react";
 import "../assets/css/Login.css";
-import Social from "../component/Login/Social"
-import { Link } from "react-router-dom";
-
-// const focus = onfocus()
-
+import Social from "../component/Login/Social";
+import { Link, useNavigate } from "react-router-dom";
+import { clearMessage } from "../slices/message";
+import { useDispatch } from "react-redux";
+import { login } from "../slices/auth";
 export default function LoginForm() {
+    /*=====[ # Declare a State Variable  # ]=====*/
 
-return (   
- <>
-   <section>
-       <div className="login-25">
-           <div className="container">
-               <div className="row login-box">
-                   <div className="col-lg-6 form-section">
-                       <div className="form-inner">
-                           <a href="login-25.html" className="logo">
-                                   <img src="assets/img/logos/logo-2.png" alt="logo" />
-                               </a>
-                               <h3>Sign Into Your Account</h3>
+    const [loginData, setLogindata] = useState({
+        username: "",
+        password: "",
+    });
+    const [passwordVisible, setPasswordVisible] = useState(false);
+    let navigate = useNavigate();
+    // const { message } = useSelector((state) => state.message);
 
-                               {/* <div className="alert alert-danger alert-dismissible fade show" role="alert">
+    const dispatch = useDispatch();
+
+    const [errors, setErrors] = useState({});
+
+    useEffect(() => {
+        if (Object.keys(errors).length != 0) {
+            setErrors({});
+        }
+    }, [loginData]);
+
+    useEffect(() => {
+        dispatch(clearMessage());
+      }, [dispatch]);
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setLogindata((prevState) => ({
+            ...prevState,
+            [name]: value,
+        }));
+    };
+
+    const togglePasswordVisibility = () => {
+        setPasswordVisible(!passwordVisible);
+    };
+
+
+    /*=====[ # Validation # ]=====*/
+
+    const validateForm = () => {
+        let validationErrors = {};
+
+        if (loginData.username == "") {
+            validationErrors.username = "User name can not be empty!";
+        }
+        else if (loginData.username.length < 4) {
+            validationErrors.username = "User name Must be 4 Character";
+        }
+        else if (loginData.password == "") {
+            validationErrors.password = "Password can not be empty!";
+        }
+
+        setErrors(validationErrors);
+
+        return Object.keys(validationErrors).length === 0;
+    };
+
+    /*=====[ # Travel plan Add Function  # ]=====*/
+
+    const handleLoginSubmit = (e) => {
+        e.preventDefault();
+
+        let isFormValid = validateForm();
+        const data ={
+            username : loginData.username,
+            password : loginData.password
+        }
+        if (isFormValid) {
+            console.log(loginData);
+            //   setLoading(true);
+
+            dispatch(login(data))
+            .unwrap()
+            .then(() => {
+                alert("success");
+              navigate("/addform");
+              window.location.reload();
+            })
+            .catch(() => {
+                alert("fail");
+            //   setLoading(false);
+            });
+        }
+    };
+
+    return (
+        <>
+            <section>
+                <div className="login-25">
+                    <div className="container">
+                        <div className="row login-box">
+                            <div className="col-lg-3"></div>
+                            <div className="col-lg-6 form-section">
+                                <div className="form-inner">
+                                    <a href="login-25.html" className="logo">
+                                        <img src="assets/img/logos/logo-2.png" alt="logo" />
+                                    </a>
+                                    <h3>Sign Into Your Account</h3>
+
+                                    {/* <div className="alert alert-danger alert-dismissible fade show" role="alert">
                                    <strong>Sorry,</strong> Something went wrong!
                                    <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                                </div> */}
 
-                               <form id="commonForm">
+                                    <form id="commonForm">
+                                        <div className="form-group position-relative clearfix">
+                                            <input
+                                                name="username"
+                                                type="text"
+                                                className="form-control "
+                                                placeholder="UserName"
+                                                aria-label=""
+                                                autoComplete="off"
+                                                value={loginData.username}
+                                                onChange={handleChange}
+                                            />
 
-                                   <div className="form-group position-relative clearfix">
+                                            <div
+                                                className="login-popover login-popover-abs"
+                                                data-bs-toggle="popover"
+                                                data-bs-trigger="hover"
+                                                title="Clarification"
+                                                data-bs-content="And here's some amazing content. It's very engaging. Right?"
+                                            >
+                                                <i className="fa fa-info-circle"></i>
+                                            </div>
+                                            <span className="text-danger">{errors.username}</span>
+                                        </div>
 
-                                       <input name="email" type="email" className="form-control " placeholder="Email Address" aria-label="Email Address" autoComplete="off" />
+                                        <div className="form-group clearfix position-relative password-wrapper">
+                                            <input
+                                                name="password"
+                                                type={passwordVisible ? 'text' : 'password'}
+                                                className="form-control"
+                                                autoComplete="off"
+                                                placeholder="Password"
+                                                aria-label="Password"
+                                                value={loginData.password}
+                                                onChange={handleChange}
+                                            />
+                                            <span onClick={togglePasswordVisibility}>
+                                                {passwordVisible ? <i className="fa fa-info-circle password-indicator"></i> :
+                                                <i className="fa fa-eye password-indicator"></i>
+                                                }
+                                            </span>
 
-                                       <div className="login-popover login-popover-abs" data-bs-toggle="popover" data-bs-trigger="hover" title="Clarification" data-bs-content="And here's some amazing content. It's very engaging. Right?">
-                                           <i className="fa fa-info-circle"></i>
-                                       </div>
-                                   </div>
+                                        </div>
+                                        <span className="text-danger">{errors.password}</span>
 
-                                   <div className="form-group clearfix position-relative password-wrapper">
-                                       <input name="password" type="password" className="form-control" autoComplete="off" placeholder="Password" aria-label="Password" />
-                                       <i className="fa fa-eye password-indicator"></i>
-                                   </div>
+                                        {/* remper */}
+                                        <div className="checkbox form-group clearfix">
+                                            <div className="form-check float-start">
+                                                <input
+                                                    className="form-check-input"
+                                                    type="checkbox"
+                                                    id="rememberme"
+                                                />
+                                                <label
+                                                    className="form-check-label"
+                                                    htmlFor="rememberme"
+                                                >
+                                                    Remember me
+                                                </label>
+                                            </div>
+                                            <Link
+                                                to="/Forgot"
+                                                className="link-light float-end forgot-password"
+                                            >
+                                                Forgot your password?
+                                            </Link>
+                                        </div>
 
+                                        <div className="form-group clearfix mb-0">
+                                            {/* <Link to="/Dashboard"> */}
+                                            <button
+                                                type="submit"
+                                                id="login"
+                                                className="btn btn-primary btn-lg btn-theme"
+                                                onClick={handleLoginSubmit}
+                                            >
+                                                Login
+                                            </button>
+                                            {/* </Link> */}
+                                        </div>
 
-                                   {/* remper */}
-                                   <div className="checkbox form-group clearfix">
-                                       <div className="form-check float-start">
-                                           <input className="form-check-input" type="checkbox" id="rememberme" />
-                                           <label className="form-check-label" htmlFor="rememberme">
-                                               Remember me
-                                           </label>
-                                       </div>
-                                       <Link to="/Forgot" className="link-light float-end forgot-password">Forgot your password?</Link>
-                                   </div>
+                                        <div className="extra-login clearfix">
+                                            <span>Or Login With</span>
+                                        </div>
+                                    </form>
 
-                                   <div className="form-group clearfix mb-0">
-                                   <Link to="/Dashboard"><button type="submit" id="login" className="btn btn-primary btn-lg btn-theme">Login</button></Link>
-                                   </div>
+                                    <div className="clearfix"></div>
 
-                                   <div className="extra-login clearfix">
-                                       <span>Or Login With</span>
-                                   </div>
-                               </form>
+                                    <Social />
 
-                               <div className="clearfix"></div>
-
-                               <Social />
-
-                               <p>Don't have an account?  <Link to="/Register">Register here</Link></p>
-
-                           </div>
-                    </div>
-                   <div className="col-lg-6 align-self-center form-text">
+                                    <p>
+                                        Don't have an account?{" "}
+                                        <Link to="/Register">Register here</Link>
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="col-lg-3"></div>
+                            {/* <div className="col-lg-6 align-self-center form-text">
                        <div className="info clearfix align-self-center">
                            <h2 className="animate-charcter">The strength of JavaScript is that you can do anything</h2>
-                           {/* <p>Programming isn't about what you know; it's about what you can figure out</p> */}
+                          
                        </div>
-                   </div>
+                   </div> */}
+                        </div>
+                    </div>
                 </div>
-            </div>
-       </div>
-    </section>
- </>
- );
+            </section>
+        </>
+    );
 }
--- server/server.js
+++ server/server.js
@@ -1,30 +1,73 @@
 const express = require("express");
-// const bodyParser = require("body-parser"); /* deprecated */
 const cors = require("cors");
+const cookieSession = require("cookie-session");
 
 const app = express();
 
-var corsOptions = {
-  origin: "http://localhost:3000"
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
+/* for Angular Client (withCredentials) */
+// app.use(
+//   cors({
+//     credentials: true,
+//     origin: ["http://localhost:8081"],
+//   })
+// );
 
 // parse requests of content-type - application/json
-app.use(express.json()); /* bodyParser.json() is deprecated */
+app.use(express.json());
 
 // parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true })); /* bodyParser.urlencoded() is deprecated */
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  cookieSession({
+    name: "Login-Auth",
+    keys: ["COOKIE_SECRET"], // should use as secret environment variable
+    httpOnly: true,
+    sameSite: 'strict'
+  })
+);
+
+// database
+const db = require("./app/models");
+// const Role = db.role;
+
+db.sequelize.sync();
+// force: true will drop the table if it already exists
+// db.sequelize.sync({force: true}).then(() => {
+//   console.log('Drop and Resync Database with { force: true }');
+//   initial();
+// });
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to application." });
+  res.json({ message: "Server is Running." });
 });
 
-require("./app/routes/tutorial.routes.js")(app);
+// routes
+require("./app/routes/auth.routes")(app);
+require("./app/routes/user.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
+  // initial();
 });
+
+// function initial() {
+//   Role.create({
+//     id: 1,
+//     name: "user",
+//   });
+
+//   Role.create({
+//     id: 2,
+//     name: "moderator",
+//   });
+
+//   Role.create({
+//     id: 3,
+//     name: "admin",
+//   });
+// }
